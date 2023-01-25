90e3b3108c575d7b5bc8d569d11d0261be0ce6e4
BabYoda <luizroberto8022@gmail.com>
2023-01-25 13:53:51 -0300

    new dev

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCV6aASsfh3UdNf5RpyFMvoWYti6cbMTJo",
+  authDomain: "chat-react-js-83427.firebaseapp.com",
+  projectId: "chat-react-js-83427",
+  storageBucket: "chat-react-js-83427.appspot.com",
+  messagingSenderId: "152889400995",
+  appId: "1:152889400995:web:9c04bebcce2d8f967fe916"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
